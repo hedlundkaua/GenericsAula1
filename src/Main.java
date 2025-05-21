24ec22a013db924e9939326498aef30971c68d23
kaua hedlund <hedlundkaua@gmail.com>
2025-05-21 14:12:44 -0300

    solução com metodo generics

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import model.entities.Product;
 import model.service.CalculationService;
 
 import java.io.BufferedReader;
@@ -5,24 +6,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
 
-        List<Integer> list = new ArrayList<>();
-
-        String path = "C:\\Windows\\Temp\\numbers.csv";
+        String path = "C:\\Windows\\Temp\\products.csv";
 
         try(BufferedReader bf = new BufferedReader(new FileReader(path))){
             String line = bf.readLine();
             while (line != null){
+            String[] fields = line.split(",");
                 //transformar line em Integer
-                list.add(Integer.parseInt(line));
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = bf.readLine();
             }
-            Integer x = CalculationService.max(list);
+            Product x = CalculationService.max(list);
             System.out.println("Max: ");
             System.out.println(x);
         }catch (IOException e){
--- src/model/service/CalculationService.java
+++ src/model/service/CalculationService.java
@@ -3,7 +3,11 @@ package model.service;
 import java.util.List;
 
 public class CalculationService {
-    public static Integer max(List<Integer> list){
+    //esse métodos vai trabalhar com qualquer tipo T
+    // desde que o T seja qualquer subtipo de comparable T
+    //usamos a linha a baixo para qualquer superclasse de Product (tipos curinga)
+    //public static <T extends Comparable<? super T>>  T max(List<T> list){
+    public static <T extends Comparable<T>>  T max(List<T> list){
 
         //programação defensiva
         //se a lista estiver vazia, mostre a exceção
@@ -11,8 +15,8 @@ public class CalculationService {
             throw new IllegalStateException("List can't be empty");
         }
         //
-        Integer max = list.get(0);
-        for(Integer item: list){
+        T max = list.get(0);
+        for(T item: list){
             //se cada item dessa lista é  maior do que o max
             //chama o primeiro "item", depois o segundo "Max"
             // e compara se for maior que zero
